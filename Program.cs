069de1a04c93e4a6860518152437f3b62453d11d
auzick <andy@uzick.com>
2022-01-26 13:22:16 -0600

    Using delegate for processing

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 
@@ -35,19 +36,21 @@ namespace JsonArrayCopier
             var writer = new JsonArrayWriter(outfile);
             writer.Start();
             var totalItems = 0;
-            totalItems += CopyArray(infile1, writer);
-            totalItems += CopyArray(infile2, writer);
+
+            totalItems += ProcessArrayFile(infile1, (record) => { writer.Write(record); });
+            totalItems += ProcessArrayFile(infile2, (record) => { writer.Write(record); });
+
             var finalFile = writer.Finish();
             var finalFileInfo = new FileInfo(finalFile);
             Console.WriteLine($"Wrote {totalItems} items ({finalFileInfo.Length}bytes) to {finalFile}");
 
         }
 
-        // https://stackoverflow.com/questions/54983533/parsing-a-json-file-with-net-core-3-0-system-text-json
-        private static int CopyArray(string inputFileName, JsonArrayWriter writer)
+
+        public static int ProcessArrayFile(string inputFileName, Action<object> function)
         {
             int itemscopied = 0;
-            Console.WriteLine($"Copying array from {inputFileName} to {writer.fileName}");
+            Console.WriteLine($"Processing array from {inputFileName}");
             using var inStream = new FileStream(inputFileName, FileMode.Open, FileAccess.Read);
             using var jsonStreamReader = new Utf8JsonStreamReader(inStream, 32 * 1024);
 
@@ -58,7 +61,7 @@ namespace JsonArrayCopier
             {
                 // deserialize object
                 var obj = jsonStreamReader.Deserialize<object>();
-                writer.Write(obj);
+                function(obj);
                 itemscopied++;
 
                 // JsonSerializer.Deserialize ends on last token of the object parsed,
@@ -70,6 +73,35 @@ namespace JsonArrayCopier
             Console.WriteLine($"{itemscopied} items copied.");
             return itemscopied;
         }
+
+        // private static int CopyArray(string inputFileName, JsonArrayWriter writer)
+        // {
+        //     int itemscopied = 0;
+        //     Console.WriteLine($"Copying array from {inputFileName} to {writer.fileName}");
+        //     using var inStream = new FileStream(inputFileName, FileMode.Open, FileAccess.Read);
+        //     using var jsonStreamReader = new Utf8JsonStreamReader(inStream, 32 * 1024);
+
+        //     jsonStreamReader.Read(); // move to array start
+        //     jsonStreamReader.Read(); // move to start of the object
+
+        //     while (jsonStreamReader.TokenType != JsonTokenType.EndArray)
+        //     {
+        //         // deserialize object
+        //         var obj = jsonStreamReader.Deserialize<object>();
+        //         writer.Write(obj);
+        //         itemscopied++;
+
+        //         // JsonSerializer.Deserialize ends on last token of the object parsed,
+        //         // move to the first token of next object
+        //         jsonStreamReader.Read();
+        //     }
+
+        //     jsonStreamReader.Dispose();
+        //     Console.WriteLine($"{itemscopied} items copied.");
+        //     return itemscopied;
+        // }
+
+
 
     }
 }
